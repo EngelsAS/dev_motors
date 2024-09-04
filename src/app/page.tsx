1ffb2bc92f19d087e5ca4cee83a8b2c0080ed6d6
EngelsAS <engelsantero@gmail.com>
2024-09-04 16:06:49 -0300

    feat: implemented hero and about section on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import { Container } from "@/components/container";
+import { Hero } from "@/components/hero";
+import { Services } from "@/components/home/services";
 import { Submenu } from "@/components/home/submenu";
 import { getDataHome } from "@/utils/actions/get-data";
 import { HomeProps } from "@/utils/types/home.type";
+import { Phone } from "lucide-react";
 
 export default async function Home() {
   const { object }: HomeProps = await getDataHome();
@@ -9,6 +13,16 @@ export default async function Home() {
   return (
     <main>
       <Submenu />
+      <Hero
+        buttonTitle={object.metadata.cta_button.title}
+        buttonUrl={object.metadata.cta_button.url}
+        bannerUrl={object.metadata.banner.url}
+        heading={object.metadata.heading}
+        icon={<Phone size={24} color="#fff" />}
+      />
+      <Container>
+        <Services object={object} />
+      </Container>
     </main>
   );
 }
---
+++ src/components/home/services/index.tsx
@@ -0,0 +1,45 @@
+import { HomeProps } from "@/utils/types/home.type";
+import styles from "./styles.module.scss";
+import Image from "next/image";
+
+export const Services = ({ object }: HomeProps) => {
+  return (
+    <>
+      <section className={styles.containerAbout}>
+        <article className={styles.innerAbout}>
+          <h1 className={styles.title}>Sobre</h1>
+          <p>{object.metadata.about.description}</p>
+        </article>
+
+        <div className={styles.bannerAbout}>
+          <Image
+            className={styles.imageAbout}
+            alt="Imagem ilustrativa sobre a empresa"
+            quality={100}
+            fill={true}
+            src={object.metadata.about.banner.url}
+          />
+        </div>
+      </section>
+
+      <h2 className={styles.servicesTitle}>Conheça nossos serviços</h2>
+
+      <section className={styles.services}>
+        {object.metadata.services.map((service, index) => (
+          <article className={styles.service} key={index}>
+            <div className={styles.innerService}>
+              <Image
+                className={styles.imageService}
+                alt="Imagem do serviço"
+                quality={100}
+                fill={true}
+                src={service.image.url}
+              />
+            </div>
+            <p>{service.description}</p>
+          </article>
+        ))}
+      </section>
+    </>
+  );
+};
